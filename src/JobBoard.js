0c1f7169ef8c7991d88258421bba81919246bd0c
RAJ HARGON <raj.hargon@gmail.com>
2020-07-22 16:49:34 +0200

    1st commit

---
+++ src/JobBoard.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { JobList } from "./JobList";
+import { loadJobs } from "./requests";
+
+export class JobBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { jobs: [] };
+  }
+  async componentDidMount() {
+    const jobs = await loadJobs();
+    console.log("JobBord", jobs);
+    this.setState({ jobs });
+  }
+  render() {
+    const { jobs } = this.state;
+    console.log(jobs);
+    return (
+      <div>
+        <h1 className="title">Job Board</h1>
+        <JobList jobs={jobs} />
+      </div>
+    );
+  }
+}
